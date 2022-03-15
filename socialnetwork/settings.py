e6d5d6e494bc86b594f95e94af7a4cb9f9ce216b
RajaKanwar <87748117+RajaKanwar@users.noreply.github.com>
2022-03-15 20:57:44 +0530

    Update settings.py

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -17,12 +17,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['s_8&hc^&p-k$ac(zwv#6c8n#hzg#)9d548wg)tcrg#16)deca!']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
