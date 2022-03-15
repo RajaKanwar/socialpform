1866a61063794d9d667d2f6ef3e77ac53d1bd72e
RajaKanwar <87748117+RajaKanwar@users.noreply.github.com>
2022-03-15 20:51:31 +0530

    Update settings.py

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["socialpform.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
